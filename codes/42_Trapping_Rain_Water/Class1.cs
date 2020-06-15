a42e1f015a96ed309a6c501e0ce36af674f2844f
Qinglin Fan <qinglin.fan@ni.com>
2020-06-16 07:32:36 +0800

    first version

--- codes/42_Trapping_Rain_Water/Class1.cs
+++ codes/42_Trapping_Rain_Water/Class1.cs
@@ -8,9 +8,10 @@ namespace _42_Trapping_Rain_Water
         {
             int ability = 0;
             bool leftBar = false;
-            for (int i=0; i < height.Length; i++)
+            int cAbility = 0;
+
+            for (int i=0; i < height.Length; )
             {
-                int cAbility = 0;
                 int bar = height[i];
                 
                 if (bar > 0)
@@ -22,23 +23,27 @@ namespace _42_Trapping_Rain_Water
                     else
                     {
                         ability += cAbility;
-
                     }
                     cAbility = 0;
                 }
                 else
                 {
-                    cAbility++;
+                    ++cAbility;
                 }
 
                 if (bar > 0) --height[i];
 
                 if ((i + 1) == height.Length)
                 {
-                    if (leftBar == false) continue;
-                    else if (cAbility == 0) continue;
-                    else i = 0;
+                    if (leftBar == false) break;
+                    else
+                    {
+                        i = 0;
+                        leftBar = false;
+                        cAbility = 0;
+                    }
                 }
+                else ++i;
       
             }
 
