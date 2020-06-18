07c69b7d083de46ee9c74121bb8a7903ae047aac
Qinglin Fan <qinglin.fan@ni.com>
2020-06-18 16:21:42 +0800

    dev

--- codes/778_Swim_in_Rising_Water/Class1.cs
+++ codes/778_Swim_in_Rising_Water/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _778_Swim_in_Rising_Water
 {
@@ -7,57 +8,76 @@ namespace _778_Swim_in_Rising_Water
         public int SwimInWater(int[][] grid)
         {
             if (grid.Length == 0 || grid.Length == 1) return 0;
-            if (grid.GetLength(0) != grid.GetLength(1)) return 0;
+            foreach (int [] row in grid)
+            {
+                if (row.Length != grid.GetLength(0)) return 0;
+            }
+ 
 
             int t = -1;
-            Swimming(ref t, 0, null, (0, 0), (grid.GetLength(0) - 1, grid.GetLength(1) - 1), grid);
+            List<(int i, int j)> path = new List<(int i, int j)>
+            {
+                (0, 0)
+            };
+            Swimming(ref t, 0, path, (0, 0), (grid.GetLength(0) - 1, grid.GetLength(0) - 1), grid);
             return (t < 0)? 0:t;
         }
 
         /*
          * time should be same to evelvation
          */
-        private void Swimming(ref int maxTime, int elevation, (int i, int j)? previous, (int i, int j) current, in (int i, int j) destination, in int[][] grid)
+        private void Swimming(ref int maxTime, int elevation, List<(int i, int j)> path, (int i, int j) current, in (int i, int j) destination, in int[][] grid)
         {
             if (current == destination)
             {
-                if (maxTime > elevation) maxTime = elevation;
+                if (maxTime < 0 || maxTime > elevation) maxTime = elevation;
                 return;
             }
 
             if (maxTime > 0 && maxTime <= elevation) return;
 
-            (int i, int j) next = (current.i, current.j);
-
             //Right
-            if (((next.i + 1 + 1) <= grid.GetLength(0)) && ((next.i+1, next.j) != previous))
+            if (((current.i + 1 + 1) <= grid.GetLength(0)) && !path.Contains((current.i + 1, current.j)))
             {
-                ++next.i;
-                if (grid[next.i][next.j] > elevation)
-                {
-                    elevation = grid[next.i][next.j];
-                }
-
-                Swimming(ref maxTime, elevation, current, next, destination, grid);
+                path.Add((current.i + 1, current.j));
+                Swimming(ref maxTime, (grid[current.i + 1][current.j] > elevation)? grid[current.i + 1][current.j] : elevation,
+                    path, (current.i + 1, current.j), destination, grid);
+                path.RemoveAt(path.Count - 1);
             }
+
+            if (maxTime > 0 && maxTime <= elevation) return;
 
             //Left
-            if (((next.i - 1) >= 0) && ((next.i - 1, next.j) != previous))
+            if (((current.i - 1) >= 0) && !path.Contains((current.i - 1, current.j)))
             {
-
+                path.Add((current.i - 1, current.j));
+                Swimming(ref maxTime, (grid[current.i - 1][current.j] > elevation) ? grid[current.i - 1][current.j] : elevation,
+                    path, (current.i - 1, current.j), destination, grid);
+                path.RemoveAt(path.Count - 1);
             }
+
+            if (maxTime > 0 && maxTime <= elevation) return;
 
             //Up
-            if (((next.j - 1) >= 0) && ((next.i, next.j - 1) != previous))
+            if (((current.j - 1) >= 0) && !path.Contains((current.i, current.j - 1)))
             {
-
+                path.Add((current.i, current.j - 1));
+                Swimming(ref maxTime, (grid[current.i][current.j-1] > elevation) ? grid[current.i][current.j-1] : elevation,
+                    path, (current.i, current.j - 1), destination, grid);
+                path.RemoveAt(path.Count - 1);
             }
+
+            if (maxTime > 0 && maxTime <= elevation) return;
 
             //Down
-            if (((next.j + 1 + 1) <= grid.GetLength(1)) && ((next.i, next.j + 1) != previous))
+            if (((current.j + 1 + 1) <= grid.GetLength(0)) && !path.Contains((current.i, current.j + 1)))
             {
-
+                path.Add((current.i, current.j + 1));
+                Swimming(ref maxTime, (grid[current.i][current.j + 1] > elevation) ? grid[current.i][current.j + 1] : elevation,
+                    path, (current.i, current.j + 1), destination, grid);
+                path.RemoveAt(path.Count - 1);
             }
+
             return;
         }
     }
---
+++ codes/778_Swim_in_Rising_WaterTests/SolutionTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _778_Swim_in_Rising_Water;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _778_Swim_in_Rising_Water.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void SwimInWaterTest()
+        {
+            Solution s = new Solution();
+            int[][] grid = new int[][]
+                {
+                    new int[] { 0, 2 },
+                    new int[] { 1, 3 },
+                };
+            Assert.IsTrue(s.SwimInWater(grid) == 3);
+        }
+
+        [TestMethod()]
+        public void SwimInWaterTest001()
+        {
+            Solution s = new Solution();
+            int[][] grid = new int[][]
+                {
+                    new int[] { 0,1,2,3,4 },
+                    new int[] { 24,23,22,21,5 },
+                    new int[] { 12,13,14,15,16 },
+                    new int[] { 11,17,18,19,20 },
+                    new int[] { 10, 9, 8, 7, 6 }
+                };
+            Assert.IsTrue(s.SwimInWater(grid) == 16);
+        }
+    }
+}
+
