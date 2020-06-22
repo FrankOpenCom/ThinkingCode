1382e63e1562348fbab089f36b648e54b08b9d1b
Qinglin Fan <qinglin.fan@ni.com>
2020-06-22 14:48:37 +0800

    last case

--- codes/778_Swim_in_Rising_Water/Class1.cs
+++ codes/778_Swim_in_Rising_Water/Class1.cs
@@ -1,79 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 
 namespace _778_Swim_in_Rising_Water
 {
     public class Solution
     {
-
-        public class LocationComp : IComparer<(int i, int j, int w)>
-        {
-            // Compares by Height, Length, and Width.
-            public int Compare((int i, int j, int w) x, (int i, int j, int w) y)
-            {
-                return x.w.CompareTo(y.w);
-            }
-        }
-
         public int SwimInWater(int[][] grid)
         {
             int[][] weights = Enumerable.Range(0, grid.Length).Select(i => Enumerable.Repeat(int.MaxValue, grid.Length).ToArray()).ToArray();
             weights[0][0] = grid[0][0];
 
-            List<(int i, int j, int w)> unVisitedQueue = new List<(int i, int j, int w)>();
+            Dictionary<(int i, int j), int> unVisitedPool = new Dictionary<(int i, int j), int>();
 
             for (int i = 0; i < grid.Length; ++i)
             {
                 for (int j = 0; j < grid.Length; ++j)
                 {
-                    unVisitedQueue.Add((i, j, int.MaxValue));
+                    unVisitedPool.Add((i, j), int.MaxValue);
                 }
             }
-            unVisitedQueue[0] = (0, 0, grid[0][0]);
-            (int i, int j, int w) node = unVisitedQueue[0];
-            unVisitedQueue.RemoveAt(0);
+            unVisitedPool[(0, 0)] = grid[0][0];
+            (int i, int j) node = unVisitedPool.Keys.ElementAt(0);
+            unVisitedPool.Remove((0, 0));
 
-            while (unVisitedQueue.Count > 0)
+            while (unVisitedPool.Count > 0)
             {
-
                 (int i, int j)[] adjacentNodes = new (int i, int j)[]
                 {
                     (node.i+1, node.j), (node.i, node.j+1),
                     (node.i-1, node.j), (node.i, node.j-1)
                 };
 
-                bool isSorted = true;
                 foreach (var (i, j) in adjacentNodes)
                 {
                     if (i < 0 || j < 0 || i >= grid.Length || j >= grid.Length) continue;
+                    if (unVisitedPool.ContainsKey((i, j)) == false) continue;
 
                     if (weights[node.i][node.j] != grid[i][j])
                     {
-                        for (int index = 0; index < unVisitedQueue.Count; ++index)
-                        {
-                            if (unVisitedQueue[index].i == i && unVisitedQueue[index].j == j)
-                            {
-                                weights[i][j] = (weights[node.i][node.j] > grid[i][j]) ? weights[node.i][node.j] : grid[i][j];
-                                unVisitedQueue[index] = (i, j, weights[i][j]);
-                                isSorted = false;
-                                break;
-                            }
-                        }
+                        weights[i][j] = (weights[node.i][node.j] > grid[i][j]) ? weights[node.i][node.j] : grid[i][j];
+                        unVisitedPool[(i, j)] = weights[i][j];
                     }
                 }
 
-                if (isSorted == false)
-                {
-                    unVisitedQueue.Sort(new LocationComp());
-                }
+                unVisitedPool = unVisitedPool.OrderBy(v => v.Value).ToDictionary(x => x.Key, x => x.Value);
 
-                node = unVisitedQueue[0];
-                unVisitedQueue.RemoveAt(0);
-
+                node = unVisitedPool.Keys.ElementAt(0);
                 if ((node.i, node.j) == (grid.Length - 1, grid.Length - 1)) 
-                    return node.w;
+                    return unVisitedPool.Values.ElementAt(0);
+
+                unVisitedPool.Remove((node.i, node.j));
+
             }
 
             return weights[grid.Length - 1][grid.Length - 1];
@@ -157,5 +135,78 @@ namespace _778_Swim_in_Rising_Water
 //    }
 //}
 
+//2.0
+//public class Solution
+//{
 
+//    public class LocationComp : IComparer<(int i, int j, int w)>
+//    {
+//        // Compares by Height, Length, and Width.
+//        public int Compare((int i, int j, int w) x, (int i, int j, int w) y)
+//        {
+//            return x.w.CompareTo(y.w);
+//        }
+//    }
 
+//    public int SwimInWater(int[][] grid)
+//    {
+//        int[][] weights = Enumerable.Range(0, grid.Length).Select(i => Enumerable.Repeat(int.MaxValue, grid.Length).ToArray()).ToArray();
+//        weights[0][0] = grid[0][0];
+
+//        List<(int i, int j, int w)> unVisitedQueue = new List<(int i, int j, int w)>();
+
+//        for (int i = 0; i < grid.Length; ++i)
+//        {
+//            for (int j = 0; j < grid.Length; ++j)
+//            {
+//                unVisitedQueue.Add((i, j, int.MaxValue));
+//            }
+//        }
+//        unVisitedQueue[0] = (0, 0, grid[0][0]);
+//        (int i, int j, int w) node = unVisitedQueue[0];
+//        unVisitedQueue.RemoveAt(0);
+
+//        while (unVisitedQueue.Count > 0)
+//        {
+
+//            (int i, int j)[] adjacentNodes = new (int i, int j)[]
+//            {
+//                    (node.i+1, node.j), (node.i, node.j+1),
+//                    (node.i-1, node.j), (node.i, node.j-1)
+//            };
+
+//            bool isSorted = true;
+//            foreach (var (i, j) in adjacentNodes)
+//            {
+//                if (i < 0 || j < 0 || i >= grid.Length || j >= grid.Length) continue;
+
+//                if (weights[node.i][node.j] != grid[i][j])
+//                {
+//                    for (int index = 0; index < unVisitedQueue.Count; ++index)
+//                    {
+//                        if (unVisitedQueue[index].i == i && unVisitedQueue[index].j == j)
+//                        {
+//                            weights[i][j] = (weights[node.i][node.j] > grid[i][j]) ? weights[node.i][node.j] : grid[i][j];
+//                            unVisitedQueue[index] = (i, j, weights[i][j]);
+//                            isSorted = false;
+//                            break;
+//                        }
+//                    }
+//                }
+//            }
+
+//            if (isSorted == false)
+//            {
+//                unVisitedQueue.Sort(new LocationComp());
+//            }
+
+//            node = unVisitedQueue[0];
+//            unVisitedQueue.RemoveAt(0);
+
+//            if ((node.i, node.j) == (grid.Length - 1, grid.Length - 1))
+//                return node.w;
+//        }
+
+//        return weights[grid.Length - 1][grid.Length - 1];
+//    }
+//}
