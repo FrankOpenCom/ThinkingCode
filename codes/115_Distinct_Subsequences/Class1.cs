ceeb6fbf28c35cced4c884bd3afc11fbd6dfb57a
Qinglin Fan <qinglin.fan@ni.com>
2020-06-23 12:48:34 +0800

    115. Distinct Subsequences

---
+++ codes/115_Distinct_Subsequences/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _115_Distinct_Subsequences
+{
+    public class Solution
+    {
+        public int NumDistinct(string s, string t)
+        {
+
+        }
+    }
+}
