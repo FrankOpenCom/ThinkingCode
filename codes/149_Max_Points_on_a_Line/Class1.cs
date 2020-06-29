b6becbdee80c7eb8380a6a07d0b1467a6b06a109
Qinglin Fan <qinglin.fan@ni.com>
2020-06-29 14:12:28 +0800

    init - 000

--- codes/149_Max_Points_on_a_Line/Class1.cs
+++ codes/149_Max_Points_on_a_Line/Class1.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
+
+// https://leetcode.com/problems/max-points-on-a-line/
 
 namespace _149_Max_Points_on_a_Line
 {
@@ -10,7 +13,37 @@ namespace _149_Max_Points_on_a_Line
     {
         public int MaxPoints(int[][] points)
         {
-            return 0;
+            if (points.Length <= 2) return points.Length;
+
+            Dictionary<(float k, float b), HashSet<(int x, int y)>> space = new Dictionary<(float k, float b), HashSet<(int x, int y)>>();
+            int maxPoints = 2;
+
+            for (int f = 0; f<points.Length; ++f)
+            {
+                for (int s=f+1; s<points.Length; ++s)
+                {
+                    // y = kx + b
+                    float k = float.NaN;
+                    float b = float.NaN;
+                    if (points[f][0] != points[s][0])
+                    {
+                        k = (points[f][1] - points[s][1]) / (points[f][0] - points[s][0]);
+                        b = points[f][1] - k * points[f][0];
+                    }
+                    else b = points[f][0];
+
+                    if (!space.ContainsKey((k, b)))
+                    {
+                        space[(k, b)] = new HashSet<(int x, int y)>();
+                    }
+
+                    space[(k, b)].Add((points[f][0], points[f][1]));
+                    space[(k, b)].Add((points[s][0], points[s][1]));
+                    if (space[(k, b)].Count > maxPoints) maxPoints = space[(k, b)].Count;
+                }
+            }
+
+            return maxPoints;
         }
     }
 }
--- codes/149_Max_Points_on_a_LineTests/SolutionTests.cs
+++ codes/149_Max_Points_on_a_LineTests/SolutionTests.cs
@@ -14,7 +14,28 @@ namespace _149_Max_Points_on_a_Line.Tests
         [TestMethod()]
         public void MaxPointsTest()
         {
-            Assert.Fail();
+            Solution S = new Solution();
+            int[][] points = new int[][]{
+                new int[] {1,1},
+                new int[] {2,2},
+                new int[] {3,3}
+            };
+            Assert.IsTrue(S.MaxPoints(points) == 3);
+        }
+
+        [TestMethod()]
+        public void MaxPointsTest001()
+        {
+            Solution S = new Solution();
+            int[][] points = new int[][]{
+                new int[] {1,1},
+                new int[] {3,2},
+                new int[] {5,3},
+                new int[] {4,1},
+                new int[] {2,3},
+                new int[] {1,4}
+            };
+            Assert.IsTrue(S.MaxPoints(points) == 4);
         }
     }
 }
