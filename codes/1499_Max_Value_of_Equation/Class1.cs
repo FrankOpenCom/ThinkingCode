061d237fec8e9261893e90d6882a1f081f6d356d
Qinglin Fan <qinglin.fan@ni.com>
2020-06-30 16:22:48 +0800

    https://leetcode.com/problems/max-value-of-equation/ a new question

---
+++ codes/1499_Max_Value_of_Equation/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// https://leetcode.com/problems/max-value-of-equation/
+
+namespace _1499_Max_Value_of_Equation
+{
+    public class Solution
+    {
+        public int FindMaxValueOfEquation(int[][] points, int k)
+        {
+            return 0;
+        }
+    }
+}
---
+++ codes/1499_Max_Value_of_EquationTests/SolutionTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1499_Max_Value_of_Equation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1499_Max_Value_of_Equation.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void FindMaxValueOfEquationTest()
+        {
+            Solution S = new Solution()
+            Assert.IsTrue(S.FindMaxValueOfEquation() == 0);
+        }
+    }
+}
