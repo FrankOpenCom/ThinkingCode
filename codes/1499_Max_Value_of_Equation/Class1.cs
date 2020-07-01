bf07239d0ae8823f859f700660825eeaadd6568e
Qinglin Fan <qinglin.fan@ni.com>
2020-07-02 07:53:40 +0800

    Heap and Backward loop

--- codes/1499_Max_Value_of_Equation/Class1.cs
+++ codes/1499_Max_Value_of_Equation/Class1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Drawing;
 
 // https://leetcode.com/problems/max-value-of-equation/
@@ -7,20 +9,68 @@ namespace _1499_Max_Value_of_Equation
 {
     public class Solution
     {
+        public class ByDiffAndIndex : IComparer<(int diff, int i)>
+        {
+            public int Compare((int diff, int i) x, (int diff, int i) y)
+            {
+                int vExt = x.diff.CompareTo(y.diff);
+                if (vExt != 0)
+                {
+                    return vExt;
+                }
+                else
+                {
+                    return x.i.CompareTo(y.i);
+                }
+            }
+        }
+
         public int FindMaxValueOfEquation(int[][] points, int k)
         {
             int max = int.MinValue;
 
-            for (int i=0; i < points.Length; ++i)
+            SortedSet<(int diff, int i)> priorities = new SortedSet<(int diff, int i)>(new ByDiffAndIndex());
+
+            priorities.Add((points[0][1] - points[0][0], points[0][0]));
+
+            for (int j = 1; j < points.Length; ++j)
+            {
+                while (priorities.Count > 0)
+                {
+                    (int diff, int i) m = priorities.Max;
+                    if ((points[j][0] - m.i) > k) priorities.Remove(m);
+                    else break;
+                }
+
+                if (priorities.Count > 0)
+                {
+                    int tMax = priorities.Max.diff + points[j][0] + points[j][1];
+                    if (tMax > max) max = tMax;
+                }
+
+                priorities.Add((points[j][1] - points[j][0], points[j][0]));
+            }
+
+            return max;
+        }
+    }
+}
+
+
+/*
+            int max = int.MinValue;
+
+            for (int i=0; i < points.Length;++i)
             {
                 for (int j=i+1; j<points.Length; ++j)
                 {
                     if (Math.Abs(points[j][0] - points[i][0]) > k) break;
                     if ((points[j][1] + points[i][1] + Math.Abs(points[j][0] - points[i][0])) > max)
+                    {
                         max = points[j][1] + points[i][1] + Math.Abs(points[j][0] - points[i][0]);
+                    }
                 }
+                
             }
-            return max;
-        }
-    }
-}
+            return max; 
+*/
--- codes/1499_Max_Value_of_EquationTests/SolutionTests.cs
+++ codes/1499_Max_Value_of_EquationTests/SolutionTests.cs
@@ -36,5 +36,77 @@ namespace _1499_Max_Value_of_Equation.Tests
             };
             Assert.IsTrue(S.FindMaxValueOfEquation(points, 3) == 3);
         }
+
+        [TestMethod()]
+        public void FindMaxValueOfEquationTest001()
+        {
+            Solution S = new Solution();
+            int[][] points = new int[][]
+            {
+                new int[] {-15,-1},
+                new int[] {-14,-5},
+                new int[] {-11,1},
+                new int[] {-9,7},
+                new int[] {-8,18},
+                new int[] {-7,-5},
+                new int[] {-3,3},
+                new int[] {4,14},
+                new int[] {12,-4},
+                new int[] {13,15},
+                new int[] {14,-19},
+                new int[] {19,-1}
+            };
+            Assert.IsTrue(S.FindMaxValueOfEquation(points, 8) == 26);
+        }
+
+        [TestMethod()]
+        public void FindMaxValueOfEquationTest002()
+        {
+            Solution S = new Solution();
+            int[][] points = new int[][]
+            {
+                new int[] {-17,-6},
+                new int[] {-4,0},
+                new int[] {-2,-16},
+                new int[] {-1,2},
+                new int[] {0,11},
+                new int[] {6,18}
+            };
+            Assert.IsTrue(S.FindMaxValueOfEquation(points, 13) == 35);
+        }
+
+        [TestMethod()]
+        public void FindMaxValueOfEquationTest003()
+        {
+            Solution S = new Solution();
+            int[][] points = new int[][]
+            {
+                new int[] {-17,5},
+                new int[] {-10,-8},
+                new int[] {-5,-13},
+                new int[] {-2,7},
+                new int[] {8,-14}
+            };
+            Assert.IsTrue(S.FindMaxValueOfEquation(points, 4) == -3);
+        }
+
+        [TestMethod()]
+        public void FindMaxValueOfEquationTest004()
+        {
+            Solution S = new Solution();
+            int[][] points = new int[][]
+            {
+                new int[] {-12,-5},
+                new int[] {-9,-6},
+                new int[] {-8,-2},
+                new int[] {-7,-20},
+                new int[] {-6,-15},
+                new int[] {-4,-20},
+                new int[] {5,3},
+                new int[] {14,-6},
+                new int[] {19,19}
+            };
+            Assert.IsTrue(S.FindMaxValueOfEquation(points, 9) == 18);
+        }
     }
 }
