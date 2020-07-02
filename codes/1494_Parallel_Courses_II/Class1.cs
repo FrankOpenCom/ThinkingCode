15bbedbe1fb861919314d662e55cbae3b4b334f6
Qinglin Fan <qinglin.fan@ni.com>
2020-07-02 09:40:42 +0800

    a new one

---
+++ codes/1494_Parallel_Courses_II/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// https://leetcode.com/problems/parallel-courses-ii/
+
+namespace _1494_Parallel_Courses_II
+{
+    public class Solution
+    {
+        public int MinNumberOfSemesters(int n, int[][] dependencies, int k)
+        {
+
+        }
+    }
+}
