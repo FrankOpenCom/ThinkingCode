2f844787cd4c1a05dc98bf1682ca76b9da01dffd
Qinglin Fan <qinglin.fan@ni.com>
2020-07-09 14:38:38 +0800

    https://leetcode.com/problems/couples-holding-hands/

---
+++ codes/765_Couples_Holding_Hands/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _765_Couples_Holding_Hands
+{
+    public class Solution
+    {
+        public int MinSwapsCouples(int[] row)
+        {
+
+        }
+    }
+}
---
+++ codes/765_Couples_Holding_HandsTests/SolutionTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _765_Couples_Holding_Hands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _765_Couples_Holding_Hands.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void MinSwapsCouplesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
