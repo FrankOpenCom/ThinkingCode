5403eaac81cc9912bdaaf6e5a776f176b0f93bda
Qinglin Fan <qinglin.fan@ni.com>
2020-07-09 13:11:22 +0800

    working

--- codes/1494_Parallel_Courses_II/Class1.cs
+++ codes/1494_Parallel_Courses_II/Class1.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 
 // https://leetcode.com/problems/parallel-courses-ii/
 
@@ -6,9 +12,111 @@ namespace _1494_Parallel_Courses_II
 {
     public class Solution
     {
+        public class Course
+        {
+            public Course(int _id)
+            {
+                id = _id;
+                depth = 0;
+                next = new List<int>();
+                prev = new List<int>();
+            }
+
+            public int Id 
+            {
+                get => id;
+            }
+
+            public int Depth
+            {
+                get => depth;
+                set
+                {
+                    depth = value;
+                }
+            }
+
+            public ref List<int> Next
+            {
+                get { return ref next; }
+            }
+
+            public ref List<int> Prev
+            {
+                get => ref prev;
+            }
+
+            private int id;
+            private int depth;
+            private List<int> next;
+            private List<int> prev;
+
+        }
+
+        void UpdatePrev(ref List<Course> courses, int id)
+        {
+            List<int> p = courses[id-1].Prev;
+            foreach (int i in p)
+            {
+                courses[i-1].Depth++;
+                UpdatePrev(ref courses, i);
+            }
+        }
+
         public int MinNumberOfSemesters(int n, int[][] dependencies, int k)
         {
+            int sessions = 0;
+            List<Course> courses = Enumerable.Range(1, n).Select(x => new Course(x)).ToList();
 
+            for (int i=0; i<dependencies.Length; ++i)
+            {
+                int left = dependencies[i][0];
+                int right = dependencies[i][1];
+
+                Course lCourse = courses[left-1];
+                Course rCourse = courses[right-1];
+
+                lCourse.Next.Add(right);
+                rCourse.Prev.Add(left);
+
+                if (rCourse.Depth < lCourse.Depth) continue;
+                else if (rCourse.Depth == lCourse.Depth) lCourse.Depth++;
+                else lCourse.Depth = rCourse.Depth + 1;
+
+                UpdatePrev(ref courses, left);
+            }
+
+            courses = courses.OrderByDescending(x => x.Depth).ThenByDescending(x => x.Next.Count).ToList();
+
+            while (courses.Count > 0)
+            {
+                int num = 0;
+                sessions++;
+                int depth = courses[0].Depth;
+                HashSet<int> completedCourses = new HashSet<int>();
+                for (int i=0; i < courses.Count && num < k; )
+                {
+                    int id = courses[i].Id;
+                    if (depth > courses[i].Depth && 
+                        (courses[i].Prev.Intersect(completedCourses).Any() || 
+                         courses.Where(x => x.Next.Exists(y => y == id)).Any()
+                        )
+                        )
+                    {
+                        ++i;
+                        continue;
+                    }
+
+                    completedCourses.Add(id);
+                    num++;
+
+                    courses.RemoveAt(i);
+                }
+            }
+
+            return sessions;
         }
     }
 }
+
+
---
+++ codes/1494_Parallel_Courses_IITests/SolutionTests.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1494_Parallel_Courses_II;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1494_Parallel_Courses_II.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void MinNumberOfSemestersTest0000()
+        {
+            Solution S = new Solution();
+            int[][] dependencies = new int[][]
+            {
+                new int[] {2,1},
+                new int[] {3,1},
+                new int[] {1,4}
+            };
+            Assert.IsTrue(S.MinNumberOfSemesters(4, dependencies, 2) == 3);
+        }
+
+        [TestMethod()]
+        public void MinNumberOfSemestersTest0001()
+        {
+            Solution S = new Solution();
+            int[][] dependencies = new int[][]
+            {
+                new int[] {2,1},
+                new int[] {3,1},
+                new int[] {4,1},
+                new int[] {1,5}
+            };
+            Assert.IsTrue(S.MinNumberOfSemesters(5, dependencies, 2) == 4);
+        }
+
+        [TestMethod()]
+        public void MinNumberOfSemestersTest0002()
+        {
+            Solution S = new Solution();
+            int[][] dependencies = new int[][]
+            {
+            };
+            Assert.IsTrue(S.MinNumberOfSemesters(11, dependencies, 2) == 6);
+        }
+
+        [TestMethod()]
+        public void MinNumberOfSemestersTest0053()
+        {
+            Solution S = new Solution();
+            int[][] dependencies = new int[][]
+            {
+                new int[] {1,2},
+                new int[] {1,3},
+                new int[] {7,5},
+                new int[] {7,6},
+                new int[] {4,8},
+                new int[] {8,9},
+                new int[] {9,10},
+                new int[] {10,11},
+                new int[] {11,12}
+            };
+            Assert.IsTrue(S.MinNumberOfSemesters(12, dependencies, 2) == 6);
+        }
+
+        [TestMethod()]
+        public void MinNumberOfSemestersTest0056()
+        {
+            Solution S = new Solution();
+            int[][] dependencies = new int[][]
+            {
+                new int[] {1,2},
+                new int[] {2,3},
+                new int[] {4,5},
+                new int[] {5,6},
+                new int[] {7,8},
+                new int[] {8,9},
+                new int[] {10,11},
+                new int[] {11,12}
+            };
+            Assert.IsTrue(S.MinNumberOfSemesters(12, dependencies, 3) == 4);
+        }
+
+        [TestMethod()]
+        public void MinNumberOfSemestersTest0057()
+        {
+            Solution S = new Solution();
+            int[][] dependencies = new int[][]
+            {
+                new int[] {1,3},
+                new int[] {1,4},
+                new int[] {2,3},
+                new int[] {2,4},
+                new int[] {5,6},
+                new int[] {6,7},
+                new int[] {7,8}
+            };
+            Assert.IsTrue(S.MinNumberOfSemesters(8, dependencies, 2) == 4);
+        }
+
+        [TestMethod()]
+        public void MinNumberOfSemestersTest0039()
+        {
+            Solution S = new Solution();
+            int[][] dependencies = new int[][]
+            {
+                new int[] {1,5},
+                new int[] {1,3},
+                new int[] {1,2},
+                new int[] {4,2},
+                new int[] {4,5},
+                new int[] {2,5},
+                new int[] {1,4},
+                new int[] {4,3},
+                new int[] {3,5},
+                new int[] {3,2}
+            };
+            Assert.IsTrue(S.MinNumberOfSemesters(5, dependencies, 3) == 5);
+        }
+
+        [TestMethod()]
+        public void MinNumberOfSemestersTest0054()
+        {
+            Solution S = new Solution();
+            int[][] dependencies = new int[][]
+            {
+                new int[] {1,5},
+                new int[] {2,5},
+                new int[] {3,5},
+                new int[] {4,6},
+                new int[] {4,7},
+                new int[] {4,8},
+                new int[] {4,9}
+            };
+            Assert.IsTrue(S.MinNumberOfSemesters(9, dependencies, 3) == 3);
+        }
+
+        [TestMethod()]
+        public void MinNumberOfSemestersTest0062()
+        {
+            Solution S = new Solution();
+            int[][] dependencies = new int[][]
+            {
+                new int[] {1,4},
+                new int[] {1,5},
+                new int[] {3,5},
+                new int[] {3,6},
+                new int[] {2,6},
+                new int[] {2,7},
+                new int[] {8,4},
+                new int[] {8,5},
+                new int[] {9,6},
+                new int[] {9,7}
+            };
+            Assert.IsTrue(S.MinNumberOfSemesters(9, dependencies, 3) == 3);
+        }
+    }
+}
