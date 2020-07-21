6af3ffce9dcc942c3fdd7e586abbc0a2c6715724
Qinglin Fan <qinglin.fan@ni.com>
2020-07-21 10:24:56 +0800

    Binary Lifting

--- codes/1483_Kth_Ancestor_Tree_Node/Class1.cs
+++ codes/1483_Kth_Ancestor_Tree_Node/Class1.cs
@@ -11,25 +11,52 @@ namespace _1483_Kth_Ancestor_Tree_Node
     {
         public TreeAncestor(int n, int[] parent)
         {
-            table = new List<List<int>>();
+            height = (int)Math.Ceiling(Math.Log(n, 2));
+            table = new List<List<int>>
+            {
+                Capacity = n
+            };
 
             for (int i = 0; i < n; ++i)
             {
-                table.Add(new List<int>() { i });
-                while (table[i].Last() != -1)
+                table.Add(new List<int>() { Capacity = height });
+            }
+
+            for (int h=0; h<height; ++h)
+            {
+                for (int i=0; i<n; ++i)
                 {
-                    table[i].Add(parent[table[i].Last()]);
+                    if (h == 0) table[i].Add(parent[i]);
+                    else if (table[i].Count >= h && table[i][h-1] != -1)
+                    {
+                        if (table[table[i][h - 1]].Count > (h - 1))
+                            table[i].Add(table[table[i][h - 1]][h - 1]);
+                        else
+                            table[i].Add(-1);
+                    }
                 }
             }
         }
 
         public int GetKthAncestor(int node, int k)
         {
-            if (table[node].Count <= k) return -1;
-            return table[node][k];
+            for (int i=0; i<height; ++i)
+            {
+                if ((k & (1 << i)) != 0)
+                {
+                    if (table[node].Count > i)
+                        node = table[node][i];
+                    else
+                        node = -1;
+                    if (node == -1) break;
+                }
+            }
+
+            return node;
         }
 
         List<List<int>> table;
+        int height;
     }
 
     /**
